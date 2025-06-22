a445b53db1fbbb4876ae11c7ceb7987c89135e7a
Spencer Saldana <dev.spencerjsaldana@gmail.com>
2025-06-22 08:36:40 -0500

    initialtest

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,33 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-const USERNAME: string = process.env.BASIC_AUTH_USERNAME ?? "";
-const PASSWORD: string = process.env.BASIC_AUTH_PASSWORD ?? "";
+// const USERNAME: string = process.env.BASIC_AUTH_USERNAME ?? "";
+// const PASSWORD: string = process.env.BASIC_AUTH_PASSWORD ?? "";
 
-export function middleware(req: NextRequest) {
-  if (!USERNAME || !PASSWORD) {
-    return NextResponse.json(
-      { error: "Basic Auth Error" },
-      {
-        headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-        status: 401,
-      }
-    );
-  }
-  const basicAuth = req.headers.get("authorization");
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
-    const [username, password] = Buffer.from(authValue, "base64")
-      .toString()
-      .split(":");
-    if (username === USERNAME && password === PASSWORD) {
-      return NextResponse.next();
-    }
-  }
-  return NextResponse.json(
-    { error: "Basic Auth Error" },
-    {
-      headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-      status: 401,
-    }
-  );
-}
+// export function middleware(req: NextRequest) {
+//   if (!USERNAME || !PASSWORD) {
+//     return NextResponse.json(
+//       { error: "Basic Auth Error" },
+//       {
+//         headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+//         status: 401,
+//       }
+//     );
+//   }
+//   const basicAuth = req.headers.get("authorization");
+//   if (basicAuth) {
+//     const authValue = basicAuth.split(" ")[1];
+//     const [username, password] = Buffer.from(authValue, "base64")
+//       .toString()
+//       .split(":");
+//     if (username === USERNAME && password === PASSWORD) {
+//       return NextResponse.next();
+//     }
+//   }
+//   return NextResponse.json(
+//     { error: "Basic Auth Error" },
+//     {
+//       headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+//       status: 401,
+//     }
+//   );
+// }
